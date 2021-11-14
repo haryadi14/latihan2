d7d154bc8f589912b7e6a6c25858b58563d3bde1
haryadi <haryadiyusuf61@gmail.com>
2021-11-14 23:31:13 +0700

    first commit

---
+++ P2_2_1204026/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1204026
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            barang barangsaya;
+            barangsaya = new barang();
+            barangsaya.nama = "Celana";
+            barangsaya.jumlahbarang = 5;
+            barangsaya.hargabarang = 40000;
+            Console.WriteLine("ADI STORE");
+            Console.WriteLine("nama barang  = {0}", barangsaya.nama);
+            Console.WriteLine("jumlah barang yang dibeli = {0} ", barangsaya.jumlahbarang);
+            Console.WriteLine("harga barangnya adalah {0} Rupiah", barangsaya.hargabarang);
+
+            
+
+
+        }
+    }
+}
---
+++ P2_2_1204026/barang.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_2_1204026
+{
+    class barang
+    {
+        public string nama;
+        public int jumlahbarang, hargabarang;
+        
+
+        public void Beli()
+        {
+            Console.WriteLine("{0} jumlah barang", nama);
+        }
+
+    }
+}
